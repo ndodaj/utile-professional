4cb5f64d10f3c89d6ad06dfdf345c9c0f433ac20
nidodaj <nikoll.dodaj@eng.it>
2024-07-15 00:22:35 +0200

    init

---
+++ src/test/java/al/utile/professional/ProfessionalApplicationTests.java
@@ -0,0 +1,13 @@
+package al.utile.professional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
