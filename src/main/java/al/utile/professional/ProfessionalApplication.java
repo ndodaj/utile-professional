95712dd9d1d70f35625ddc68ff35c50794efe554
nidodaj <nikoll.dodaj@eng.it>
2024-07-31 14:07:06 +0200

    fix professional domain

--- src/main/java/al/utile/professional/ProfessionalApplication.java
+++ src/main/java/al/utile/professional/ProfessionalApplication.java
@@ -2,12 +2,10 @@ package al.utile.professional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableFeignClients
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class ProfessionalApplication {
 
 	public static void main(String[] args) {
